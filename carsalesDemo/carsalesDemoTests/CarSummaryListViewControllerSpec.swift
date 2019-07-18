7af6cc822cae4e285e9f3f2ff6b2d1827d311683
tom wang <tomwang2045@gmail.com>
2019-07-18 20:35:09 +1000

    Build fix

---
+++ carsalesDemo/carsalesDemoTests/CarSummaryListViewControllerSpec.swift
@@ -0,0 +1,17 @@
+//
+//  CarSummaryListViewControllerSpec.swift
+//  carsalesDemoTests
+//
+//  Created by syle on 18/7/19.
+//  Copyright © 2019 tom. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import carsalesDemo
+
+class CarSummaryListViewControllerSpec: QuickSpec {
+    override func spec() {
+    }
+}
