4a1e6bbf7be76860777a58bffa64f880813d4720
tom wang <tomwang2045@gmail.com>
2019-07-18 20:25:40 +1000

    Add unit tests for new viewModel

--- carsalesDemo/carsalesDemoTests/carsalesDemoTests.swift
+++ carsalesDemo/carsalesDemoTests/carsalesDemoTests.swift
@@ -11,35 +11,60 @@ import XCTest
 
 class carsalesDemoTests: XCTestCase {
 
+    var viewModel: CarSummariesViewModel!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    func testCarsummaryViewModel() {
-        let carsummary = CarSummary(Id: "id", Title: "title", Location: "location", Price: "price", MainPhoto: "photo", DetailsUrl: "url")
-        let viewModel = CarSummaryViewModel(carSummary: carsummary)
+        let mockSession = MockCarSummarySession()
+        let carService = CarService(session: mockSession)
+        viewModel = CarSummariesViewModel(carService: carService)
+        let exceptation = XCTestExpectation(description: "Fetch car summaries")
         
-        XCTAssertTrue(viewModel.Title == "title")
-        XCTAssertTrue(viewModel.Location == "location")
-        XCTAssertTrue(viewModel.Price == "price")
-        XCTAssertTrue(viewModel.MainPhoto == "photo")
-        XCTAssertTrue(viewModel.DetailsUrl == "url")
+        viewModel.fetchCarSummaries { (error) in
+                exceptation.fulfill()
+        }
+        
+        self.wait(for: [exceptation], timeout: 10.0)
     }
+    
+    func testFetchCarSummaries() {
+        let exceptation = XCTestExpectation(description: "Fetch car summaries")
+        
+        viewModel.fetchCarSummaries { (error) in
+            if error == nil {
+                XCTAssert(error == nil)
+                exceptation.fulfill()
+            }else{
+                XCTFail()
+            }
+        }
+        
+        self.wait(for: [exceptation], timeout: 10.0)
+    }
+
+    func testGetCarSummariesCount() {
+        XCTAssert(viewModel.getCarSummariesCount() == 2)
+    }
+    
+    func testGetCarTitle() {
+        XCTAssert(viewModel.getCarTitle(with: 0) == "2013 BMW 328i Sport Line F30 Auto MY14")
+    }
+    
+    func testGetCarLocation() {
+        XCTAssert(viewModel.getCarLocation(with: 0) == "VIC")
+    }
+    
+    func testGetCarPrice() {
+        XCTAssert(viewModel.getCarPrice(with: 0) == "$30,480")
+    }
+    
+    func testGetCarMainPhoto() {
+        XCTAssert(viewModel.getCarMainPhoto(with: 0) == "https://carsales.li.csnstatic.com/car/dealer/bdtuvorf4cvft0nrun3xtt6e5.jpg")
+    }
+    
+    func testGetCarDetailUrl() {
+        XCTAssert(viewModel.getCarDetailUrl(with: 0) == "/stock/car/test/v1/details/OAG-AD-17216859")
+    }
+
     
     func testCarDetailViewModel() {
         let photos = ["1"]
