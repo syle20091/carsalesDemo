e39e6ffc2a385070c78d3e43c0bbae935c0b511b
tom wang <tomwang2045@gmail.com>
2019-07-23 17:17:08 +1000

    Add alternative way to decode cardetailJson

--- carsalesDemo/carsalesDemo/Model/CarDetail.swift
+++ carsalesDemo/carsalesDemo/Model/CarDetail.swift
@@ -26,3 +26,35 @@ struct CarOverView: Decodable {
 struct CarDetailResult: Decodable {
     var Result: [CarDetail]?
 }
+
+struct CarDetailView {
+    var Id: String?
+    var SaleStatus: String?
+    var Location: String?
+    var Price: String?
+    var Photos: [String]?
+    var Comments: String?
+}
+
+
+extension CarDetailView: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case Result
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let resluts = try container.decode([CarDetail].self, forKey: .Result)
+        
+        guard let result = resluts.first else {
+            return
+        }
+        
+        Id = result.Id
+        SaleStatus = result.SaleStatus
+        Comments = result.Comments
+        Location = result.Overview?.Location
+        Price = result.Overview?.Price
+        Photos = result.Overview?.Photos
+    }
+}
